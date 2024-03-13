de6a69ee821949314b98371e942397f62bf7ba34
przemyslawren <przemyslaw.ren@gmail.com>
2024-03-13 13:14:22 +0100

    add Average method

---
+++ console-app/ConsoleApp.cs
@@ -0,0 +1,27 @@
+﻿
+namespace new_app;
+
+internal static class ConsoleApp
+{
+    public static void Main(string[] args)
+    {
+        const string name = "Przemek";
+        Console.WriteLine("Hello!");
+        Console.WriteLine($"My name is {name}");
+        int[] arr = { 1, 2, 4, 6, 8, 10};
+        var arrContents = string.Join(", ", arr);
+        Console.WriteLine(arrContents);
+        Console.WriteLine("Average of the array above is: " + Average(arr));
+
+        static double Average(int[] arr)
+        {
+            double sum = 0;
+            for (var i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum / arr.Length;
+        }
+    }
+}
