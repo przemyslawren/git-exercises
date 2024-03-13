5814ac26ff7910890bc86c8d32ac5519904f0b0a
przemyslawren <przemyslaw.ren@gmail.com>
2024-03-13 14:00:15 +0100

    add Max method

--- console-app/ConsoleApp.cs
+++ console-app/ConsoleApp.cs
@@ -8,10 +8,13 @@ internal static class ConsoleApp
         const string name = "Przemek";
         Console.WriteLine("Hello!");
         Console.WriteLine($"My name is {name}");
-        int[] arr = { 1, 2, 4, 6, 8, 10};
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         var arrContents = string.Join(", ", arr);
         Console.WriteLine(arrContents);
         Console.WriteLine("Average of the array above is: " + Average(arr));
+        
+        Console.WriteLine(arrContents);
+        Console.WriteLine($"Max number in the array above is: {Max(arr)}");
 
         static double Average(int[] arr)
         {
@@ -22,6 +25,20 @@ internal static class ConsoleApp
             }
 
             return sum / arr.Length;
+        }
+
+        static double Max(int[] arr)
+        {
+            int max = 0;
+            foreach (var i in arr)
+            {
+                if (i > max)
+                {
+                    max = i;
+                }
+            }
+
+            return max;
         }
     }
 }
